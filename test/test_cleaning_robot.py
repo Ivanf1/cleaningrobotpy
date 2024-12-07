ef344f54778f453fd7a84c1857e940cd52553c60
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-12-07 10:26:05 +0100

    red phase US2.1 #2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -12,3 +12,11 @@ class TestCleaningRobot(TestCase):
         c = CleaningRobot()
         c.initialize_robot()
         self.assertEqual(c.robot_status(), "(0,0,N)")
+
+    @patch.object(GPIO, "output")
+    @patch.object(IBS, "get_charge_left")
+    def test_should_turn_off_the_cleaning_system_and_turn_on_the_led_when_battery_lower_than_10(self, mock_ibs: Mock, mock_gpio: Mock):
+        c = CleaningRobot()
+        mock_ibs.return_value = 5
+        c.manage_cleaning_system()
+        mock_gpio.assert_has_calls([call(c.CLEANING_SYSTEM_PIN, GPIO.LOW), call(c.RECHARGE_LED_PIN, GPIO.HIGH)])
