4f8dfc0639f78ccb4b4ea0f840c09d9d8dd68132
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-12-07 11:36:17 +0100

    green phase US7.1, additional test #7

--- test/test_room.py
+++ test/test_room.py
@@ -8,3 +8,7 @@ class TestRoom(TestCase):
     def test_should_return_position_valid(self):
         r = Room(2, 2)
         self.assertTrue(r.is_position_valid((1, 1)))
+
+    def test_should_return_position_invalid(self):
+        r = Room(2, 2)
+        self.assertFalse(r.is_position_valid((3, 1)))
