ae5f70b0ba1829173594aa661e166ae6f01b92e6
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-12-07 11:47:49 +0100

    red phase US8.2 #8

--- test/test_display_manager.py
+++ test/test_display_manager.py
@@ -12,3 +12,9 @@ class TestDisplayManager(TestCase):
         d = DisplayManager(mock.board.I2C())
         d.update_display_info((0,0,'N'), None, 20)
         mock_display.assert_called_once_with("R: (0,0,N) - O: ***** - B: 20%")
+
+    @patch.object(HD44780, "lcd_string")
+    def test_should_display_robot_position_obstacle_position_battery_percentage(self, mock_display: Mock):
+        d = DisplayManager(mock.board.I2C())
+        d.update_display_info((0,0,'N'), (0,1), 42)
+        mock_display.assert_called_once_with("R: (0,0,N) - O: (0,1) - B: 42%")
