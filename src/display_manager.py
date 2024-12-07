c3acaf0f788d2b2e15162b308aaf291386bf8bf2
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-12-07 11:47:30 +0100

    green phase US8.1 #8

--- src/display_manager.py
+++ src/display_manager.py
@@ -10,4 +10,9 @@ class DisplayManager:
         self.display = HD44780(i2c)
 
     def update_display_info(self, robot_position: tuple[int, int, str], obstacle_position: tuple[int, int] or None, battery: int):
-        pass
+        rx, ry, rh = robot_position
+        if obstacle_position is None:
+            obstacle_str = "*****"
+
+        self.display.lcd_clear()
+        self.display.lcd_string(f"R: ({rx},{ry},{rh}) - O: {obstacle_str} - B: {battery}%")
