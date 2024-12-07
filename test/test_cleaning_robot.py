1bca64932b4944552f1366f7c0b2cf4b466ff7ab
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-12-07 11:40:43 +0100

    red phase US7.2 #7

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -4,19 +4,22 @@ from unittest.mock import Mock, patch, call
 from mock import GPIO
 from mock.ibs import IBS
 from src.cleaning_robot import CleaningRobot, CleaningRobotError
+from src.room import Room
 
 
 class TestCleaningRobot(TestCase):
 
     def test_should_reset_robot_position(self):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         self.assertEqual(c.robot_status(), "(0,0,N)")
 
     @patch.object(GPIO, "output")
     @patch.object(IBS, "get_charge_left")
     def test_should_turn_off_the_cleaning_system_and_turn_on_the_led_when_battery_lower_than_10(self, mock_ibs: Mock, mock_gpio: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         mock_ibs.return_value = 5
         c.manage_cleaning_system()
         mock_gpio.assert_has_calls([call(c.CLEANING_SYSTEM_PIN, GPIO.LOW), call(c.RECHARGE_LED_PIN, GPIO.HIGH)])
@@ -24,14 +27,16 @@ class TestCleaningRobot(TestCase):
     @patch.object(GPIO, "output")
     @patch.object(IBS, "get_charge_left")
     def test_should_turn_on_the_cleaning_system_and_turn_off_the_led_when_battery_greater_than_10(self, mock_ibs: Mock, mock_gpio: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         mock_ibs.return_value = 12
         c.manage_cleaning_system()
         mock_gpio.assert_has_calls([call(c.CLEANING_SYSTEM_PIN, GPIO.HIGH), call(c.RECHARGE_LED_PIN, GPIO.LOW)])
 
     @patch.object(IBS, "get_charge_left")
     def test_should_move_forward(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_ibs.return_value = 12
         c.execute_command(c.FORWARD)
@@ -39,7 +44,8 @@ class TestCleaningRobot(TestCase):
 
     @patch.object(IBS, "get_charge_left")
     def test_should_move_left(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_ibs.return_value = 12
         c.execute_command(c.LEFT)
@@ -47,7 +53,8 @@ class TestCleaningRobot(TestCase):
 
     @patch.object(IBS, "get_charge_left")
     def test_should_rotate_right(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_ibs.return_value = 12
         c.execute_command(c.RIGHT)
@@ -55,14 +62,16 @@ class TestCleaningRobot(TestCase):
 
     @patch.object(IBS, "get_charge_left")
     def test_should_raise_error_on_invalid_movement_command(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_ibs.return_value = 12
         self.assertRaises(CleaningRobotError, c.execute_command, "U")
 
     @patch.object(IBS, "get_charge_left")
     def test_should_raise_error_on_invalid_movement_out_of_bound(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_ibs.return_value = 12
         c.execute_command(c.LEFT)
@@ -71,7 +80,8 @@ class TestCleaningRobot(TestCase):
     @patch.object(IBS, "get_charge_left")
     @patch.object(GPIO, "input")
     def test_should_not_move_forward_if_obstacle_found(self, mock_gpio: Mock, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_gpio.return_value = True
         mock_ibs.return_value = 12
@@ -79,7 +89,8 @@ class TestCleaningRobot(TestCase):
 
     @patch.object(IBS, "get_charge_left")
     def test_should_turn_on_the_cleaning_system_and_turn_off_the_led(self, mock_ibs: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         mock_ibs.return_value = 9
         c.initialize_robot()
         self.assertEqual(c.execute_command(c.FORWARD), "!(0,0,N)")
@@ -88,7 +99,8 @@ class TestCleaningRobot(TestCase):
     @patch.object(IBS, "get_charge_left")
     @patch.object(GPIO, "input")
     def test_should_trigger_the_buzzer_if_obstacle_found(self, mock_gpio: Mock, mock_ibs: Mock, mock_buzzer: Mock):
-        c = CleaningRobot()
+        r = Room(2, 2)
+        c = CleaningRobot(r)
         c.initialize_robot()
         mock_gpio.return_value = True
         mock_ibs.return_value = 12
